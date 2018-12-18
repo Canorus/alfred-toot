ed9e03e9432f16be7e5a0593299dac51a80edd4b
Canorus <helios7157@gmail.com>
2018-12-18 20:51:37 +0900

    \#1

--- reply.py
+++ reply.py
@@ -9,6 +9,6 @@ info = load(open('info.plist','rb'))
 access = info['variables']['access_key']
 instance = info['variables']['instance']
 head = {'Authorization':'Bearer '+access}
-data['status'] = '@'+json.loads(requests.get(instance+'/api/v1/statuses/'+data['in_reply_to_id']).content.decode('utf-8'))['account']['acct']+' '+data['status']
+data['status'] = '@'+json.loads(requests.get(instance+'/api/v1/statuses/'+data['in_reply_to_id']).content.decode('utf-8'))['account']['acct']+' '+unicodedata.normalize('NFC',data['status'])
 t = requests.post(instance+'/api/v1/statuses',data=data,headers=head)
 print(json.loads(requests.get(instance+'/api/v1/statuses/'+data['in_reply_to_id']).content.decode('utf-8'))['account']['acct'])
