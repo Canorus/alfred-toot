ceb166c08352d332440b499c78695d48b680b37e
Canorus <helios7157@gmail.com>
2019-06-04 00:32:30 +0900

    - 답장의 경우 모든 참여자에 대하여 멘션 #6

--- fetch_notifications.py
+++ fetch_notifications.py
@@ -1,4 +1,4 @@
--*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 import sys
 from plistlib import load
 import requests
@@ -10,21 +10,22 @@ try:
     reply = sys.argv[1]
 except:
     reply = ""
-info = load(open('info.plist','rb'))
+info = load(open('info.plist', 'rb'))
 access = info['variables']['access_key']
 instance = info['variables']['instance']
-head = {'Authorization':'Bearer '+access}
+head = {'Authorization': 'Bearer '+access}
 
-notis = json.loads(requests.get(instance+'/api/v1/notifications',headers=head).content.decode('utf-8'))
+notis = json.loads(requests.get(
+    instance+'/api/v1/notifications', headers=head).content.decode('utf-8'))
 
 def strip(t):
-    t = re.sub('</p><p>','\n',t)
-    t = re.sub('(<.?p>|<.?a.*?>|<.?span.*?>)','',t)
-    t = re.sub('&lt;','<',t)
-    t = re.sub('&gt;','>',t)
-    t = re.sub('&apos;','\'',t)
-    t = re.sub('&quot;','\'',t)
-    t = re.sub('<br.*?\/?>','\n',t)
+    t = re.sub('</p><p>', '\n', t)
+    t = re.sub('(<.?p>|<.?a.*?>|<.?span.*?>)', '', t)
+    t = re.sub('&lt;', '<', t)
+    t = re.sub('&gt;', '>', t)
+    t = re.sub('&apos;', '\'', t)
+    t = re.sub('&quot;', '\'', t)
+    t = re.sub('<br.*?\/?>', '\n', t)
     return t
 
 items = list()
@@ -34,11 +35,11 @@ for noti in notis:
         item["title"] = strip(noti['status']['content'])
         item["subtitle"] = "from: "+str(noti['account']['acct'])
         if noti['status']['mentions']:
-		mention = ['@'+i['acct'] for i in noti['status']['mentions']]
-		mention.append(str(noti['account']['acct']))
-	else:
-		mention = []
-        item["arg"] = '{"status":"'+str(reply)+'","in_reply_to_id":"'+str(noti['status']['id'])+'","acct":"'+str(mention)+'"}' # list 어떻게 넘겨야 되지
+            mention = ['@'+i['acct'] for i in noti['status']['mentions']]
+            mention.append('@'+str(noti['account']['acct']))
+        else:
+            mention = []
+        item["arg"] = '{"status":"'+str(reply)+'","in_reply_to_id":"'+str(noti['status']['id'])+'","acct":"'+' '.join(mention)+'"}'  # list 어떻게 넘겨야 되지
         items.append(item)
-results = {"items":items}
+results = {"items": items}
 print(json.dumps(results))
--- reply.py
+++ reply.py
@@ -9,6 +9,7 @@ info = load(open('info.plist','rb'))
 access = info['variables']['access_key']
 instance = info['variables']['instance']
 head = {'Authorization':'Bearer '+access}
-whoami = json.loads(requests.get(instance+'/api/v1/accounts/verify_credentials',headers=head).content.deode('utf-8'))['acct']
+whoami = '@'+json.loads(requests.get(instance+'/api/v1/accounts/verify_credentials',headers=head).content.decode('utf-8'))['acct'] + ' '
+data['acct'] = data['acct'].replace(whoami,'')
 data['status'] = str(data['acct'])+' '+unicodedata.normalize('NFC',data['status'])
 t = requests.post(instance+'/api/v1/statuses',data=data,headers=head)
